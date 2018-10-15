46af3cdd6f40c2b5095c954917acb202997dcc30
Pulkit Aggarwal <aggarwalpulkit596@gmail.com>
2018-10-16 01:16:47 +0530

    Update BaseActivity.kt

--- app/src/main/java/com/hachiko/hachiko/BaseActivity.kt
+++ app/src/main/java/com/hachiko/hachiko/BaseActivity.kt
@@ -8,8 +8,10 @@ open class BaseActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        //no need to set full screen in base activity can be done using app theme 
+        
+//         window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+//                 WindowManager.LayoutParams.FLAG_FULLSCREEN)
 
         supportActionBar!!.hide()
     }
