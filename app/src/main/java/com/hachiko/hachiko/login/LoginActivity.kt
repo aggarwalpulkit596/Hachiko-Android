2a108d44c99feb5d04aa1e074f5b4b79b59ee31d
Rahul Ray <rahul9650ray@gmail.com>
2018-10-16 01:26:40 +0530

    google sign in done

--- app/src/main/java/com/hachiko/hachiko/login/LoginActivity.kt
+++ app/src/main/java/com/hachiko/hachiko/login/LoginActivity.kt
@@ -4,18 +4,29 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.app.ActionBar
+import android.util.Log
 import android.view.View
 import android.view.WindowManager
 import android.widget.Toast
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 import com.hachiko.hachiko.BaseActivity
 import com.hachiko.hachiko.R
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : BaseActivity() {
 
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
 
         loginBtn.setOnClickListener(View.OnClickListener {
             val intent=Intent(this,OTPActivity::class.java)
@@ -24,5 +35,6 @@ class LoginActivity : BaseActivity() {
         })
 
     }
+
 
 }
